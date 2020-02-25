d4195da56e283af85092d7087cefd733dfd37058
Plamen Pentchev <plamenpentchev@yahoo.com>
2020-02-25 13:46:46 +0100

    secure login added

--- ASC.Web/ClientApp/src/app/app.module.ts
+++ ASC.Web/ClientApp/src/app/app.module.ts
@@ -12,12 +12,14 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { SecureHomeComponent } from 'src/app/secure-home/secure-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    SecureHomeComponent,
     CounterComponent,
     FetchDataComponent
   ],
@@ -28,6 +30,7 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     ApiAuthorizationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path:'dashboard', component:SecureHomeComponent, pathMatch: 'full'},
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
     ])
