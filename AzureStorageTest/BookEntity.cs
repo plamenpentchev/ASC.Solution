03f67729a0be3a512a0f7f998ec6342c54bcb446
Plamen Pentchev <plamenpentchev@yahoo.com>
2020-02-27 15:04:15 +0100

    AzureStorageSimulator project added

---
+++ AzureStorageTest/BookEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureStorageTest
+{
+    /// <summary>
+    /// a book entity that will be used to perform
+    /// sample data operations against the Storage emulator
+    /// </summary>
+    class BookEntity:BaseEntity
+    {
+        public BookEntity()
+        {
+
+        }
+        public BookEntity(int bookId, string publisher)
+        {
+            this.RowKey = bookId.ToString();
+            this.PartitionKey = publisher;
+        }
+
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+    }
+}
---
+++ AzureStorageTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureStorageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Azure Storage account and Table Service Instances
+            CloudStorageAccount storageAccount;
+            CloudTableClient tableClient;
+
+            //connect to storage account
+            storageAccount = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+
+            //create the table 'Book' if it not exists
+            tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable cloudTable = tableClient.GetTableReference("Book");
+            cloudTable.CreateIfNotExistsAsync();
+
+            BookEntity aNewBook = new BookEntity() { Author = "Rami", BookName = "ASP.NET Core with Azure", Publisher = "APress" };
+            aNewBook.BookId = 2;
+            aNewBook.RowKey = aNewBook.BookId.ToString();
+            aNewBook.PartitionKey = aNewBook.Publisher;
+            aNewBook.CreatedDate = DateTime.UtcNow;
+            aNewBook.UpdatedDate = DateTime.UtcNow;
+
+            //insert and execute operations
+            TableOperation insertOperation = TableOperation.Insert(aNewBook);
+            cloudTable.ExecuteAsync(insertOperation);
+
+            Console.ReadLine();
+
+        }
+    }
+}
